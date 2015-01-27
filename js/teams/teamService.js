b979bae12ec7d1a0405dc1afc73f2eb2ae2443b3
skootdiggity <smmiller.work@gmail.com>
2015-01-26 17:02:22 -0700

    through step 2

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,47 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj) {
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		var homeTeamScore = parseInt(gameObj.homeTeamScore);
+		var opponentScore = parseInt(gameObj.opponentScore);
+		var deferred = $q.defer();
+		if(homeTeamScore > opponentScore) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		};
+		return $http({
+			method: 'POST',
+			url: url,
+			data: gameObj
+
+		});
+	};
+
+	this.getTeamData = function(team) {
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data) {
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++) {
+				if(results[i].won === true) {
+					wins += 1;
+				} else {
+					losses += 1;
+				};
+			};
+			results.wins = wins;
+			results.losses = losses;
+		})
+		return deferred.promise(results);
+
+	};
+
+
 });
