e2f247fac368b3b39048d5e50d5d32b4b43ff8eb
skootdiggity <smmiller.work@gmail.com>
2015-01-28 16:03:26 -0700

    FINISHED the NBA!!!!!

--- js/home/homeService.js
+++ js/home/homeService.js
@@ -4,21 +4,25 @@ app.service('homeService', function($http, $q, teamService){
 
 	this.getHomeTmplData = function() {
 		var deferred = $q.defer();
+		var teamsArray = [0, 1, 2];
 		var homeTmplTeams = {};
 		teamService.getTeamData('utahjazz').then(function(results) {
 			homeTmplTeams.utahjazz = results;
 			homeTmplTeams.utahjazz.name = 'Utah Jazz';
 			homeTmplTeams.utahjazz.logo = 'images/jazz-logo.png';
-			teamService.getTeamData('miamiheat').then(function(results) {
-				homeTmplTeams.miamiheat = results;
-				homeTmplTeams.miamiheat.name = 'Miami Heat';
-				homeTmplTeams.miamiheat.logo = 'images/heat-logo.png';
-				teamService.getTeamData('losangeleslakers').then(function(results) {
-					homeTmplTeams.losangeleslakers = results;
-					homeTmplTeams.losangeleslakers.name = 'Los Angeles Lakers';
-					homeTmplTeams.losangeleslakers.logo = 'images/lakers-logo.png'
+			teamsArray[0] = homeTmplTeams.utahjazz;
+			teamService.getTeamData('losangeleslakers').then(function(results) {
+				homeTmplTeams.losangeleslakers = results;
+				homeTmplTeams.losangeleslakers.name = 'Los Angeles Lakers';
+				homeTmplTeams.losangeleslakers.logo = 'images/lakers-logo.png'
+				teamsArray[1] = homeTmplTeams.losangeleslakers;
+				teamService.getTeamData('miamiheat').then(function(results) {
+					homeTmplTeams.miamiheat = results;
+					homeTmplTeams.miamiheat.name = 'Miami Heat';
+					homeTmplTeams.miamiheat.logo = 'images/heat-logo.png';
+					teamsArray[2] = homeTmplTeams.miamiheat;
 
-					deferred.resolve(homeTmplTeams);
+					deferred.resolve(teamsArray);
 				});
 			});
 		});
