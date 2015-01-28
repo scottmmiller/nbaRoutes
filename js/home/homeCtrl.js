fb5b81af27fe1c5fb7e6a7b01c1cd05512ab810f
skootdiggity <smmiller.work@gmail.com>
2015-01-28 13:57:25 -0700

    fixed homeTmpl home team names

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -3,6 +3,7 @@ var app = angular.module('nbaRoutes');
 app.controller('homeCtrl', function($scope, $routeParams, homeService, homeTmplData){
 	homeService.getHomeTmplData().then(function(results) {
 		$scope.teamData = results;
+		console.log($scope.teamData)
 	})
 
 });
--- js/home/homeService.js
+++ js/home/homeService.js
@@ -7,17 +7,18 @@ app.service('homeService', function($http, $q, teamService){
 		var homeTmplTeams = {};
 		teamService.getTeamData('utahjazz').then(function(results) {
 			homeTmplTeams.utahjazz = results;
+			homeTmplTeams.utahjazz.name = 'Utah Jazz';
 			homeTmplTeams.utahjazz.logo = 'images/jazz-logo.png';
 			teamService.getTeamData('miamiheat').then(function(results) {
 				homeTmplTeams.miamiheat = results;
+				homeTmplTeams.miamiheat.name = 'Miami Heat';
 				homeTmplTeams.miamiheat.logo = 'images/heat-logo.png';
 				teamService.getTeamData('losangeleslakers').then(function(results) {
 					homeTmplTeams.losangeleslakers = results;
+					homeTmplTeams.losangeleslakers.name = 'Los Angeles Lakers';
 					homeTmplTeams.losangeleslakers.logo = 'images/lakers-logo.png'
 
 					deferred.resolve(homeTmplTeams);
-							console.log(homeTmplTeams)
-
 				});
 			});
 		});
